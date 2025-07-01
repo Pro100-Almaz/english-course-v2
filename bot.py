776d8372ad018c3a2a0174f2ef8c0e288be36092
Almaz <q19.almaz@gmail.com>
2025-07-01 20:20:50 +0500

    update message handler priority"

--- bot.py
+++ bot.py
@@ -11,11 +11,9 @@ async def on_startup(_):
 def on_shutdown(_):
     print('Бот сдох')
 
-client.handlers_register(dp)
 manage.handlers_register_manage(dp)
 payment.handlers_register(dp)
+client.handlers_register(dp)
 common.register_common_handlers(dp)
-
-
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown)
